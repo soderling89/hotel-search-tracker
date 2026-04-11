ac2370da46113d3311f451ad02ab25636ebbe306
soderling89 <emanuel.sodero@fravega.com.ar>
2026-04-11 14:03:24 -0300

    initial project configuration with spring initializr

---
+++ src/main/java/com/emanuel/sodero/hotel_search_tracker/HotelSearchTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.emanuel.sodero.hotel_search_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelSearchTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelSearchTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emanuel/sodero/hotel_search_tracker/HotelSearchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuel.sodero.hotel_search_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelSearchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
